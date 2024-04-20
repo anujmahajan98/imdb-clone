2c371119dccc480c26024b52ba39aab3834d933b
anujmahajan98 <53164451+anujmahajan98@users.noreply.github.com>
2024-04-20 18:00:02 -0400

    Dark and Light Mode Functionality

---
+++ src/app/components/DarkModeToggle.jsx
@@ -0,0 +1,47 @@
+"use client"
+
+import React from 'react'
+
+import { MdLightMode, MdDarkMode } from 'react-icons/md'
+import { useTheme } from 'next-themes'
+import { useState } from 'react'
+
+export default function DarkModeToggle() {
+
+    const{theme, setTheme, systemTheme} = useTheme()
+    const currentTheme = theme === 'system' ? systemTheme : theme
+
+    const [isChecked, setIsChecked] = useState(currentTheme === 'dark');
+
+    const toggleTheme = () => {
+        setIsChecked(!isChecked);
+        setTheme(isChecked ? 'light' : 'dark');
+    };
+
+    return (
+        // <div>
+        //     {currentTheme === 'dark' ? ( 
+        //     <MdLightMode onClick={() => setTheme('light')} className='text-xl cursor-pointer hover:amber-text-500'/> 
+        //     ) : ( 
+        //     <MdDarkMode onClick={() => setTheme('dark')} className='text-xl cursor-pointer hover:amber-text-500'/>
+        // )}
+        // </div>
+        <div className="relative">
+            <input
+                type="checkbox"
+                id="darkModeToggle"
+                className="hidden"
+                checked={isChecked}
+                onChange={toggleTheme}
+            />
+            <label htmlFor="darkModeToggle" className="cursor-pointer">
+                <div className="w-12 h-7 bg-gray-400 rounded-full shadow-inner hover:text-amber-400">
+                {/* <div className={`absolute left-0 top-0 w-6 h-6 bg-white rounded-full shadow transition-transform transform ${isChecked ? 'translate-x-4' : 'translate-x-0'}`}> */}
+                    <div className={`absolute left-2 top-1.5 w-6 h-6 shadow transition-transform transform ${isChecked ? 'translate-x-4' : 'translate-x-0'}`}>
+                    {currentTheme === 'dark' ? <MdLightMode /> : <MdDarkMode />}
+                    </div>
+                </div>
+            </label>
+        </div>
+    )
+}
--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -3,6 +3,7 @@ import MenuItem from './MenuItem'
 import { AiFillHome } from 'react-icons/ai'
 import { BsFillInfoCircleFill } from 'react-icons/bs'
 import Link from 'next/link'
+import DarkModeToggle from './DarkModeToggle'
 
 export default function Header() {
   return (
@@ -11,10 +12,13 @@ export default function Header() {
             <MenuItem title = 'Home' address = "/" Icon = {AiFillHome}/>
             <MenuItem title = 'About' address = "/about" Icon = {BsFillInfoCircleFill}/>
         </div>
-        <Link href={"/"} className='flex gap-1 items-center'>
-            <span className='text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg'>IMDb</span>
-            <span className='text-xl hidden sm:inline'>Clone</span>
-        </Link>
+        <div className='flex items-center gap-4'>
+            <DarkModeToggle />
+            <Link href={"/"} className='flex gap-1 items-center'>
+                <span className='text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg'>IMDb</span>
+                <span className='text-xl hidden sm:inline'>Clone</span>
+            </Link>
+        </div>
     </div>
   )
 }
