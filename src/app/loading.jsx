891c385e78a35e77da52824a8d616a091962c0c7
anujmahajan98 <53164451+anujmahajan98@users.noreply.github.com>
2024-04-21 22:10:48 -0400

    Added Loading Spinner

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function loading() {
+  return (
+    <div className='flex justify-center mt-20'>
+        <img src='loadingSpinner.svg' alt='Loading ...' />
+    </div>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,23 @@ const TMDB_API_KEY = process.env.TMDB_API_KEY
 
 export default async function Home( { searchParams } ) {
   const genre = searchParams.genre || 'fetchTrending';
-  const res = await fetch( 
-    `https://api.themoviedb.org/3${
-      genre === `fetchTopRated` ? `/movie/top_rated` : `/trending/all/week`}?api_key=${TMDB_API_KEY}&
-      language=en-US&page=1`
-  );
+  // const res = await fetch( 
+  //   `https://api.themoviedb.org/3${
+  //     genre === `fetchTopRated` ? `/movie/top_rated` : `/trending/all/week`}?api_key=${TMDB_API_KEY}&
+  //     language=en-US&page=1`
+  // );
+
+  const res = await new Promise((resolve) => {
+    setTimeout(async() => {
+      const response = fetch( 
+      `https://api.themoviedb.org/3${
+        genre === `fetchTopRated` ? `/movie/top_rated` : `/trending/all/week`}?api_key=${TMDB_API_KEY}&
+        language=en-US&page=1`,
+        {next : {revalidate : 10000}}
+    );
+    resolve(response);
+    }, 2000);
+  });
 
   console.log(res)
 
