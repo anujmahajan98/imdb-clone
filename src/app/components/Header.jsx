7ea0fd7935c0e6ca04c9ac750d47543a1da0da07
anujmahajan98 <53164451+anujmahajan98@users.noreply.github.com>
2024-04-21 20:03:55 -0400

    Fetched movie data from TMDB API

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -8,16 +8,17 @@ import DarkModeToggle from './DarkModeToggle'
 export default function Header() {
   return (
     <div className='flex justify-between items-center p-3 max-w-6xl mx-auto'>
-        <div className='flex gap-4 items-center'>
+        <div className='flex gap-10 items-center'>
             {/* <MenuItem title = 'Home' address = "/" Icon = {AiFillHome}/> */}
             <Link href={"/"} className='flex gap-1 items-center'>
                 <span className='text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg'>IMDb</span>
                 <span className='text-xl hidden sm:inline'>Clone</span>
             </Link>
+            <MenuItem title = 'About' address = "/about" Icon = {BsFillInfoCircleFill}/>
             
         </div>
         <div className='flex items-center gap-10'>
-            <MenuItem title = 'About' address = "/about" Icon = {BsFillInfoCircleFill}/>
+            
             <DarkModeToggle />
         </div>
     </div>
