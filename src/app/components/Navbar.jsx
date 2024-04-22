0f6b88c8969d0fc14922e158d3fb34b62c400d69
anujmahajan98 <53164451+anujmahajan98@users.noreply.github.com>
2024-04-22 18:50:54 -0400

    Fixed the suspense issue

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import NavbarItem from './NavbarItem'
 
 export default function Navbar() {
   return (
-    <div className='flex dark:bg-gray-500 bg-amber-100 p-5 lg:text-lg justify-center gap-6'>
-        <NavbarItem title = "Trending" param = "fetchTrending" />
-        <p className='ml-3 mr-3'>|</p>
-        <NavbarItem title = "Top Rated" param = "fetchTopRated" />
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className='flex dark:bg-gray-500 bg-amber-100 p-5 lg:text-lg justify-center gap-6'>
+          <NavbarItem title = "Trending" param = "fetchTrending" />
+          <p className='ml-3 mr-3'>|</p>
+          <NavbarItem title = "Top Rated" param = "fetchTopRated" />
+      </div>
+    </Suspense>
   )
 }
